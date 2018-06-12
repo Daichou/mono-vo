d376ff966769447825fc1fd64d5a108a22b7c656
Daichou <tommy0705c@gmail.com>
2018-06-12 11:07:12 +0000

    add fast hdmi

---
+++ src/fast_hdmi.h
@@ -0,0 +1,21 @@
+
+#ifndef _FAST_HDMI_H_
+#define _FAST_HDMI_H_
+
+#include <iostream>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <linux/fb.h>
+#include <linux/videodev2.h>
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+using namespace std;
+
+void hdmi_init();
+static void hdmi_show(Mat& outputimage);
+
+#endif
