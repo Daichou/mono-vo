9f430f663e5ea9c5846bd4966b83b168429b3a52
Ian Chen <ianre657@gmail.com>
2018-06-24 17:10:24 +0800

    ignore compiled files

--- opencv-3.1.0/build/modules/python/.python/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/opencv-3.1.0/modules/python
-# Build directory: /home/ubuntu/opencv-3.1.0/build/modules/python/.python
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(python2)
-SUBDIRS(python3)
